981821426706b0b347cd01e5f01de18217a6b0e2
EP Lin <pingeplin@gmail.com>
2023-03-28 09:52:46 +0800

    feat: Add history and push to gpt

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = typer.Typer()
 load_dotenv()
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
-openai.organization = os.getenv('OPENAI_ORGANIZATION')
+history = []
 
 
 def signal_handler(sig, frame):
@@ -22,37 +22,47 @@ signal.signal(signal.SIGTERM, signal_handler)
 
 
 def get_prompt():
-    print('\rYou: ', end='')
+    print('\rYou: ')
     prompt = ''
+
+    # Limit history to 6 messages
+    if len(history) >= 6:
+        history.pop(0)
+
     while True:
         temp = input()
-        if temp.strip() == 'EOF':
+        if temp.strip().upper() == 'EOF':
             break
 
         prompt += temp
+
+    prompt = prompt.strip()
+    history.append({'role': 'user', 'content': prompt})
     return prompt
 
 
 def ask_gpt(prompt):
-    if prompt.strip() == '':
+    if prompt == '':
         return
 
     response = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
-        messages=[
-            {'role': 'user', 'content': prompt.strip()},
-        ],
+        messages=history,
+        temperature=1.5,
         stream=True
     )
 
-    print('\rGPT: ', end='')
+    print('\rGPT: ')
+    result = []
     for chunk in response:
         delta: dict = chunk['choices'][0]['delta']
         if delta and delta.get('content'):
             content: str = delta['content']
+            result.append(content)
             print(content, end='', flush=True)
         if not delta:
             print('\n')
+    history.append({'role': 'assistant', 'content': ' '.join(result)})
 
 
 @app.command()
